4e8a78c8193b0405c03d587b2aa6c8e726a78169
Sean Mullen <srmullen13@gmail.com>
2020-04-12 13:48:10 -0600

    minification, render throttling

--- index.js
+++ index.js
@@ -1,88 +1,9 @@
 import './style.scss';
 import { GPU } from 'gpu.js';
 import dat from 'dat.gui';
-import { loadImage, createCanvas, saveImage, uploadImage, removeElement, unsplashUrl } from './utils';
-
-function rgb2hsv(red, green, blue) {
-  const cmax = Math.max(Math.max(red, green), blue);
-  const cmin = Math.min(Math.min(red, green), blue);
-  const delta = cmax - cmin;
-
-  let hue = 0;
-  if (delta === 0) {
-    // hue is 0
-  } else if (cmax === red) {
-    hue = 60 * (((green - blue) / delta) % 6);
-  } else if (cmax === green) {
-    hue = 60 * (((blue - red) / delta) + 2)
-  } else if (cmax === blue) {
-    hue = 60 * (((red - green) / delta) + 4)
-  }
-
-  let sat = 0
-  if (cmax === 0) {
-    // sat is 0
-  } else {
-    sat = delta / cmax
-  }
-
-  const val = cmax;
-
-  return [hue, sat, val];
-}
-
-function hsv2rgb(hue, sat, val) {
-  const c = val * sat;
-  const x = c * (1 - Math.abs(((hue / 60) % 2) - 1));
-  const m = val - c;
-
-  let r = 0;
-  let g = 0;
-  let b = 0;
-  if (0 <= hue && hue < 60) {
-    r = c;
-    g = x;
-    b = 0;
-  } else if (60 <= hue && hue < 120) {
-    r = x;
-    g = c;
-    b = 0;
-  } else if (120 <= hue && hue < 180) {
-    r = 0;
-    g = c;
-    b = x;
-  } else if (180 <= hue && hue < 240) {
-    r = 0;
-    g = x;
-    b = c;
-  } else if (240 <= hue && hue < 300) {
-    r = x;
-    g = 0;
-    b = c;
-  } else {
-    r = c;
-    g = 0;
-    b = x;
-  }
-  return [r + m, g + m, b + m];
-}
-
-function hsvKernel(image, hueRot, saturationOffset, valueOffset) {
-  const pixel = image[this.thread.y][this.thread.x];
-  const red = pixel[0];
-  const green = pixel[1];
-  const blue = pixel[2];
-  
-  let [hue, sat, val] = rgb2hsv(red, green, blue);
-
-  hue = (hue + hueRot) % 360;
-  sat = Math.max(0, Math.min(1, sat + saturationOffset));
-  val = Math.max(0, Math.min(1, val + valueOffset));
-
-  // Now change back to RGB
-  let [r, g, b] = hsv2rgb(hue, sat, val);
-  this.color(r, g, b, pixel[3]);
-}
+import throttle from 'lodash.throttle';
+import { hsv2rgb, rgb2hsv, hsvKernel } from './kernels';
+import { loadImage, createCanvas, saveImage, uploadImage, removeElement } from './utils';
 
 function setupKernel(image) {
   const canvasContainer = document.getElementById('canvas-container');
@@ -91,10 +12,9 @@ function setupKernel(image) {
     style: 'max-height: 80vh; max-width: 100%;'
   });
 
-  // Need to preserve the drawing buffer for ability to save images.
-  // const gl = canvas.getContext('webgl2', { preserveDrawingBuffer: true });
   let gpu = new GPU({
     canvas,
+    // Need to preserve the drawing buffer for ability to save images.
     context: canvas.getContext('webgl2', { preserveDrawingBuffer: true })
   });
 
@@ -143,15 +63,13 @@ function setupKernel(image) {
     download: () => saveImage(kernel.canvas)
   }
 
-  gui.add(hsv, 'hue', 0, 360).onChange(() => {
+  const onChange = throttle(() => {
     kernel(image, hsv.hue, hsv.saturation, hsv.value);
-  });
-  gui.add(hsv, 'saturation', -1, 1).step(0.01).onChange(() => {
-    kernel(image, hsv.hue, hsv.saturation, hsv.value);
-  });
-  gui.add(hsv, 'value', -1, 1).step(0.01).onChange(() => {
-    kernel(image, hsv.hue, hsv.saturation, hsv.value);
-  });
+  }, 50);
+
+  gui.add(hsv, 'hue', 0, 360).onChange(onChange);
+  gui.add(hsv, 'saturation', -1, 1).step(0.01).onChange(onChange);
+  gui.add(hsv, 'value', -1, 1).step(0.01).onChange(onChange);
   gui.add(fns, 'download');
 
   kernel(image, hsv.hue, hsv.saturation, hsv.value);
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OpimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 module.exports = env => {
   return {
-    devtool: 'eval-source-map',
-    mode: 'development',
+    devtool: 'source-map',
+    mode: 'production',
     entry: './index.js',
     output: {
       path: path.resolve(__dirname, './dist'),
       filename: "bundle.js"
     },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserJSPlugin({}), new OpimizeCSSAssetsPlugin({})]
+    },
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
       })
     ],
     module: {
@@ -23,15 +34,14 @@ module.exports = env => {
         {
           test: /\.s[ac]ss$/i,
           use: [
-            'style-loader',
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
             'css-loader',
             'sass-loader'
           ]
         }
       ]
-    },
-    devServer: {
-      contentBase: __dirname
     }
   };
 }
